c7db5e1ab82006fe0f25027dfbfda69492918b9c
YusufTBaysal <yt_baysal06@hotmail.com>
2022-03-03 00:41:13 +0300

    works

---
+++ Work1/Work1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product ürün1 = new Product();
+            ürün1.ShoeBrand = "Nike";
+            ürün1.ShoeSize = "45";
+
+            Product ürün2 = new Product();
+            ürün2.TshirtBrand = "POLO";
+            ürün2.TshirtBodySize = "XL";
+
+            Console.WriteLine("Brand : " + ürün1.ShoeBrand + ", Size : " + ürün1.ShoeSize);
+            Console.WriteLine("Brand : " + ürün2.TshirtBrand + ", Body Size : " + ürün2.TshirtBodySize);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Product
+    {
+        public string ShoeSize { get; set; }
+        public string ShoeBrand { get; set; }
+        public string TshirtBrand { get; set; }
+        public string TshirtBodySize { get; set; }
+    }
+
+}
