b52a1106c10db31123aa16f5dcd0faa5229d3e7f
YusufTBaysal <yt_baysal06@hotmail.com>
2022-03-05 02:18:23 +0300

    Works

---
+++ GenericIntro/GenericIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Yusuf");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Talha");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP1/Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "ahmet", "mehmet", "cengiz", "ali" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("veli");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
